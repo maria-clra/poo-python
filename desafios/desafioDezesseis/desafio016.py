cd07945643538b28a3cadae3fd36cc6a34facdb1
Maria Clara <mclaranb@icloud.com>
2026-02-24 17:42:36 -0300

    Empresa dentro da classe

--- desafios/desafioDezesseis/desafio016.py
+++ desafios/desafioDezesseis/desafio016.py
@@ -1,15 +1,17 @@
 from rich import print
+from rich import inspect
 
 class Funcionario:
-    def __init__(self, nome, setor, cargo, empresa = "Curso em Video"):
+    empresa = "Curso em Video"
+
+    def __init__(self, nome, setor, cargo):
         self.nome = nome
         self.setor = setor
         self.cargo = cargo
-        self.empresa = empresa
 
     def apresentacao(self):
-        return f":handshake: Olá, sou {self.nome}, e sou {self.cargo} do setor {self.setor} da empresa {self.empresa}."
-
+        return f":handshake: Olá, sou {self.nome}, e sou {self.cargo} do setor {self.setor} da empresa {self.__class__.empresa}."
+    
 
 c1 = Funcionario("Maria", "Administação", "Diretora")
 c2 = Funcionario("Diego", "Engenharia", "Desenhista")
