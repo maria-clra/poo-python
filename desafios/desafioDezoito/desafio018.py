958994a2b0d90dd745951e85f5dee8c6c089211a
Maria Clara <mclaranb@icloud.com>
2026-03-02 16:14:36 -0300

    corrigindo o código com a aula nova: divisão, funções, atributos de classe

--- desafios/desafioDezoito/desafio018.py
+++ desafios/desafioDezoito/desafio018.py
@@ -3,25 +3,33 @@ from rich import inspect
 from rich import print
 
 class Churrasco:
+    carne:float = 0.400
+    kg:float = 82.40
+
     def __init__(self, titulo, quant):
         self.titulo = titulo
         self.quant = quant
 
-    def analisar(self):
+    def quant_carne(self) -> float:
+        return self.quant * Churrasco.carne
+    
+    def custo_total(self) -> float:
+        return self.quant_carne() * self.__class__.kg
+    
+    def custo_individual(self) -> float:
+        return self.custo_total() / self.quant
         #Consumo padrão: 400g por pessoa
         #Preço: 82,40/Kg
-        
-        carne = 0.4
-        kg = 82.40
-        comprar = carne * self.quant
-        total = kg * comprar
-        valorPessoa = total % self.quant
-
+   
+        comprar = Churrasco.carne * self.quant
+        total = Churrasco.kg * comprar
+        valorPessoa = total / self.quant
+    def analisar(self):
         conteudo = f"Analisando [green]{self.titulo}[/] com [blue]{self.quant} convidados[/]\n"
-        conteudo += f"Cada participante comerá {carne}Kg e cada Kg custa R${kg:.2f}\n"
-        conteudo += f"Recomendo [blue]comprar {comprar}Kg[/] de carne\n"
-        conteudo += f"O custo total será de [green]R${total:.2f}[/] \n"
-        conteudo += f"Cada pessoa pagará [yellow]R${valorPessoa:.2f}[/] para participar"
+        conteudo += f"Cada participante comerá {Churrasco.carne}Kg e cada Kg custa R${Churrasco.kg:.2f}\n"
+        conteudo += f"Recomendo [blue]comprar {self.quant_carne()}Kg[/] de carne\n"
+        conteudo += f"O custo total será de [green]R${self.custo_total():,.2f}[/] \n"
+        conteudo += f"Cada pessoa pagará [yellow]R${self.custo_individual():,.2f}[/] para participar"
 
         print(Panel(conteudo, title="Churras dos Amigos"))
   
