9c6f751ccf0419f658d9233f20f8e2e88ae7399c
Maria Clara <mclaranb@icloud.com>
2026-03-05 13:13:10 -0300

    tentando

--- desafios/desafioDezenove/desafio019.py
+++ desafios/desafioDezenove/desafio019.py
@@ -2,13 +2,23 @@ from rich import print
 
 class Livro:
 
-    def __init__(self, titulo, paginas):
+    def __init__(self, titulo, paginas_total):
         self.titulo= titulo
-        self.paginas = paginas
-
-    def avancar_paginas(self):
-        return f":book: Você acabou de abrir o livro [red]'{self.titulo}'[/] que tem [green]{self.paginas} páginas[/] no total"
-
-
+        self.paginas_total = paginas_total
+        self.pagina_atual = 0
+        print (f":book: Você acabou de abrir o livro [red]'{self.titulo}'[/] que tem [green]{self.paginas_total} páginas[/] no total.")
+    
+    def avancar_paginas(self, paginas):
+        if self.pagina_atual + paginas <= self.paginas_total:
+            self.pagina_atual += paginas
+            print(f"Você avaçou {paginas} páginas e agora está na página {self.pagina_atual}") 
+        
+        else:
+            self.pagina_atual = self.paginas_total
+            print(f":rotating_light: Você chegou ao final do livro {self.titulo}")
+        
+        
+        
 l1 = Livro("10 coisas que aprendi", 20)
-print(l1.avancar_paginas())
+l1.avancar_paginas(5)
+l1.avancar_paginas(5)
