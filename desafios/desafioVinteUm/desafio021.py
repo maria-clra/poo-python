2d2527eec4492eb1d537e41884683d6f47de5956
Maria Clara <mclaranb@icloud.com>
2026-03-16 15:11:03 -0300

    fazendo sozinha

--- desafios/desafioVinteUm/desafio021.py
+++ desafios/desafioVinteUm/desafio021.py
@@ -1,17 +1,36 @@
+from rich import print
+
 class Caneta: 
+    tampada = True
 
     def __init__(self,cor):
         self.cor = cor
 
-    def destampar():
-        #só escreve se estiver destampada
+    def tampar(self):
+        self.tampada = True
+          
+    def destampar(self):
+        self.tampada = False
 
-    def escrever(conteudo):
-        #escreve as coisas que colocar no conteudo
+    def escrever(self, conteudo):
+        if not self.tampada:
+            if self.cor == "vermelha":
+                print(f"[red]{conteudo}")
+            elif self.cor == "verde":
+                print(f"[green]{conteudo}")
+            elif self.cor == "azul":
+                print(f"[blue]{conteudo}")
 
-    def quebrar_linha():
-        #quebra linha entre um conteudo e outro
+    def quebrar_linha(self):
+        print("\n")
 
 c1 = Caneta("vermelha")
-c1 = Caneta("verde")
-c1 = Caneta("azul")
+c2 = Caneta("verde")
+c3 = Caneta("vermelha")
+
+c2.destampar()
+c2.escrever("oi")
+c3.escrever("sei")
+c3.tampar()
+c3.escrever("oi")
+c3.escrever("sei")
