9d66fb42a376c82818642a98c052aa287b0ab8ec
Maria Clara <mclaranb@icloud.com>
2026-03-16 14:38:10 -0300

    molde do que é pedido no desafio: encapsulamento

---
+++ desafios/desafioVinteUm/desafio021.py
@@ -0,0 +1,17 @@
+class Caneta: 
+
+    def __init__(self,cor):
+        self.cor = cor
+
+    def destampar():
+        #só escreve se estiver destampada
+
+    def escrever(conteudo):
+        #escreve as coisas que colocar no conteudo
+
+    def quebrar_linha():
+        #quebra linha entre um conteudo e outro
+
+c1 = Caneta("vermelha")
+c1 = Caneta("verde")
+c1 = Caneta("azul")
